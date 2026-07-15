fc0feb4191be85eb43b15dd5dc6c0f5b9e452a6f
Maurizio <2711967@gmail.com>
2026-07-15 09:05:17 +0200

    Aggiornato compileSdk e targetSdk a 35 per requisiti Google Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.chantalbortolussi.tattooapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.chantalbortolussi.tattooapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
